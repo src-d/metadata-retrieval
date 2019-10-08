aaf051a6014303d2eb5ac8c223eca741139bdb69
Maxim Sukharev <max@smacker.ru>
2019-10-08 18:39:19 +0200

    Use go-bindata for migrations
    Signed-off-by: Maxim Sukharev <max@smacker.ru>

--- database/database.go
+++ database/database.go
@@ -3,14 +3,22 @@ package database
 import (
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
-	_ "github.com/golang-migrate/migrate/v4/source/file"
+	bindata "github.com/golang-migrate/migrate/v4/source/go_bindata"
 )
 
 // Migrate updates the DB schema to the latest version
 func Migrate(databaseURL string) error {
-	m, err := migrate.New(
-		"file://database/migrations",
-		databaseURL)
+	s := bindata.Resource(AssetNames(),
+		func(name string) ([]byte, error) {
+			return Asset(name)
+		})
+
+	d, err := bindata.WithInstance(s)
+	if err != nil {
+		return err
+	}
+
+	m, err := migrate.NewWithSourceInstance("go-bindata", d, databaseURL)
 	if err != nil {
 		return err
 	}
