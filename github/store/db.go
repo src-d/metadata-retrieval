706093cc9cd7bc355c8e8b406b30f13303112264
Maxim Sukharev <max@smacker.ru>
2019-10-14 12:03:37 +0200

    Tidy github provider
    1. Remove not needed fields like `billing_email`
    2. Remove non-exiting fields like `clone_url`
    3. Use golint compatible names everywhere
    
    Ref: #35
    
    Signed-off-by: Maxim Sukharev <max@smacker.ru>

--- github/store/db.go
+++ github/store/db.go
@@ -36,9 +36,9 @@ func (s *DB) Version(v int) {
 }
 
 const (
-	organizationsCols             = "avatar_url, billing_email, collaborators, created_at, description, email, htmlurl, id, location, login, name, node_id, owned_private_repos, public_repos, total_private_repos, two_factor_requirement_enabled, updated_at"
-	usersCols                     = "avatar_url, bio, company, created_at, email, followers, following, hireable, htmlurl, id, location, login, name, node_id, owned_private_repos, private_gists, public_gists, public_repos, site_admin, total_private_repos, updated_at"
-	repositoriesCols              = "allow_merge_commit, allow_rebase_merge, allow_squash_merge, archived, clone_url, created_at, default_branch, description, disabled, fork, forks_count, full_name, has_issues, has_wiki, homepage, htmlurl, id, language, mirror_url, name, node_id, open_issues_count, owner_id, owner_login, owner_type, private, pushed_at, sshurl, stargazers_count, topics, updated_at, watchers_count"
+	organizationsCols             = "avatar_url, collaborators, created_at, description, email, htmlurl, id, login, name, node_id, owned_private_repos, public_repos, total_private_repos, updated_at"
+	usersCols                     = "avatar_url, bio, company, created_at, email, followers, following, hireable, htmlurl, id, location, login, name, node_id, owned_private_repos, private_gists, public_gists, public_repos, total_private_repos, updated_at"
+	repositoriesCols              = "allow_merge_commit, allow_rebase_merge, allow_squash_merge, archived, created_at, default_branch, description, disabled, fork, forks_count, full_name, has_issues, has_wiki, homepage, htmlurl, id, language, name, node_id, open_issues_count, owner_id, owner_login, owner_type, private, pushed_at, sshurl, stargazers_count, topics, updated_at, watchers_count"
 	issuesCols                    = "assignees, body, closed_at, closed_by_id, closed_by_login, comments, created_at, htmlurl, id, labels, locked, milestone_id, milestone_title, node_id, number, repository_name, repository_owner, state, title, updated_at, user_id, user_login"
 	issueCommentsCols             = "author_association, body, created_at, htmlurl, id, issue_number, node_id, repository_name, repository_owner, updated_at, user_id, user_login"
 	pullRequestsCol               = "additions, assignees, author_association, base_ref, base_repository_name, base_repository_owner, base_sha, base_user, body, changed_files, closed_at, comments, commits, created_at, deletions, head_ref, head_repository_name, head_repository_owner, head_sha, head_user, htmlurl, id, labels, maintainer_can_modify, merge_commit_sha, mergeable, merged, merged_at, merged_by_id, merged_by_login, milestone_id, milestone_title, node_id, number, repository_name, repository_owner, review_comments, state, title, updated_at, user_id, user_login"
@@ -144,10 +144,10 @@ func (s *DB) SaveOrganization(ctx context.Context, organization *graphql.Organiz
 		`INSERT INTO organizations_versioned
 		(sum256, versions, %s)
 		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14,
-			$15, $16, $17, $18, $19)
+			$15, $16)
 		ON CONFLICT (sum256)
 		DO UPDATE
-		SET versions = array_append(organizations_versioned.versions, $20)`,
+		SET versions = array_append(organizations_versioned.versions, $17)`,
 		organizationsCols)
 
 	st := fmt.Sprintf("%+v", organization)
@@ -158,26 +158,20 @@ func (s *DB) SaveOrganization(ctx context.Context, organization *graphql.Organiz
 		hashString,
 		pq.Array([]int{s.v}),
 
-		organization.AvatarUrl, // avatar_url text,
-		// TODO
-		"",                                        // organization.OrganizationBillingEmail, // billing_email text,
+		organization.AvatarURL,                    // avatar_url text,
 		organization.MembersWithRole.TotalCount,   // collaborators bigint,
 		organization.CreatedAt,                    // created_at timestamptz,
 		organization.Description,                  // description text,
 		organization.Email,                        // email text,
-		organization.Url,                          // htmlurl text,
-		organization.DatabaseId,                   // id bigint,
-		organization.Location,                     // location text,
+		organization.URL,                          // htmlurl text,
+		organization.DatabaseID,                   // id bigint,
 		organization.Login,                        // login text,
 		organization.Name,                         // name text,
-		organization.Id,                           // node_id text,
+		organization.ID,                           // node_id text,
 		organization.OwnedPrivateRepos.TotalCount, // owned_private_repos bigint,
 		organization.PublicRepos.TotalCount,       // public_repos bigint,
 		organization.TotalPrivateRepos.TotalCount, // total_private_repos bigint,
-		// TODO: requires admin privileges
-		//organization.RequiresTwoFactorAuthentication, // two_factor_requirement_enabled boolean,
-		false,
-		organization.UpdatedAt, // updated_at timestamptz,
+		organization.UpdatedAt,                    // updated_at timestamptz,
 
 		s.v,
 	)
@@ -193,10 +187,10 @@ func (s *DB) SaveUser(ctx context.Context, user *graphql.UserExtended) error {
 		`INSERT INTO users_versioned
 		(sum256, versions, %s)
 		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14,
-			$15, $16, $17, $18, $19, $20, $21, $22, $23)
+			$15, $16, $17, $18, $19, $20, $21, $22)
 		ON CONFLICT (sum256)
 		DO UPDATE
-		SET versions = array_append(users_versioned.versions, $24)`,
+		SET versions = array_append(users_versioned.versions, $23)`,
 		usersCols)
 
 	st := fmt.Sprintf("%+v", user)
@@ -207,7 +201,7 @@ func (s *DB) SaveUser(ctx context.Context, user *graphql.UserExtended) error {
 		hashString,
 		pq.Array([]int{s.v}),
 
-		user.AvatarUrl, // avatar_url text,
+		user.AvatarURL, // avatar_url text,
 		user.Bio,       // bio text,
 		user.Company,   // company text,
 		user.CreatedAt, // created_at timestamptz,
@@ -216,18 +210,17 @@ func (s *DB) SaveUser(ctx context.Context, user *graphql.UserExtended) error {
 		user.Followers.TotalCount,         // followers bigint,
 		user.Following.TotalCount,         // following bigint,
 		user.IsHireable,                   // hireable boolean,
-		user.Url,                          // htmlurl text,
-		user.DatabaseId,                   // id bigint,
+		user.URL,                          // htmlurl text,
+		user.DatabaseID,                   // id bigint,
 		user.Location,                     // location text,
 		user.Login,                        // login text,
 		user.Name,                         // name text,
-		user.Id,                           // node_id text,
+		user.ID,                           // node_id text,
 		user.OwnedPrivateRepos.TotalCount, // owned_private_repos bigint,
 		// TODO: gists makes the server return: You don't have permission to see gists.
 		0,                                 // private_gists bigint,
 		0,                                 // public_gists bigint,
 		user.PublicRepos.TotalCount,       // public_repos bigint,
-		user.IsSiteAdmin,                  // site_admin boolean,
 		user.TotalPrivateRepos.TotalCount, // total_private_repos bigint,
 		user.UpdatedAt,                    // updated_at timestamptz,
 
@@ -246,10 +239,10 @@ func (s *DB) SaveRepository(ctx context.Context, repository *graphql.RepositoryF
 		(sum256, versions, %s)
 		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14,
 			$15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29,
-			$30, $31, $32, $33, $34)
+			$30, $31, $32)
 		ON CONFLICT (sum256)
 		DO UPDATE
-		SET versions = array_append(repositories_versioned.versions, $35)`,
+		SET versions = array_append(repositories_versioned.versions, $33)`,
 		repositoriesCols)
 
 	st := fmt.Sprintf("%+v %v", repository, topics)
@@ -264,7 +257,6 @@ func (s *DB) SaveRepository(ctx context.Context, repository *graphql.RepositoryF
 		repository.RebaseMergeAllowed,    // allow_rebase_merge boolean
 		repository.SquashMergeAllowed,    // allow_squash_merge boolean
 		repository.IsArchived,            // archived boolean
-		repository.Url,                   // clone_url text
 		repository.CreatedAt,             // created_at timestamptz
 		repository.DefaultBranchRef.Name, // default_branch text
 		repository.Description,           // description text
@@ -274,20 +266,19 @@ func (s *DB) SaveRepository(ctx context.Context, repository *graphql.RepositoryF
 		repository.NameWithOwner,         // full_name text
 		repository.HasIssuesEnabled,      // has_issues boolean
 		repository.HasWikiEnabled,        // has_wiki boolean
-		repository.HomepageUrl,           // homepage text
-		repository.Url,                   // htmlurl text
-		repository.DatabaseId,            // id bigint,
+		repository.HomepageURL,           // homepage text
+		repository.URL,                   // htmlurl text
+		repository.DatabaseID,            // id bigint,
 		repository.PrimaryLanguage.Name,  // language text
-		repository.MirrorUrl,             // mirror_url text
 		repository.Name,                  // name text
-		repository.Id,                    // node_id text
+		repository.ID,                    // node_id text
 		repository.OpenIssues.TotalCount, // open_issues_count bigint
 		repoOwnerID(repository),          // owner_id bigint NOT NULL,
 		repository.Owner.Login,           // owner_login text NOT NULL,
 		repository.Owner.Typename,        // owner_type text NOT NULL
 		repository.IsPrivate,             // private boolean
 		repository.PushedAt,              // pushed_at timestamptz
-		repository.SshUrl,                // sshurl text
+		repository.SSHURL,                // sshurl text
 		repository.Stargazers.TotalCount, // stargazers_count bigint
 		pq.Array(topics),                 // topics text[] NOT NULL
 		repository.UpdatedAt,             // updated_at timestamptz
@@ -305,9 +296,9 @@ func (s *DB) SaveRepository(ctx context.Context, repository *graphql.RepositoryF
 func repoOwnerID(repository *graphql.RepositoryFields) int {
 	switch repository.Owner.Typename {
 	case "Orgazation":
-		return repository.Owner.Organization.DatabaseId
+		return repository.Owner.Organization.DatabaseID
 	case "User":
-		return repository.Owner.User.DatabaseId
+		return repository.Owner.User.DatabaseID
 	default:
 		return 0
 	}
@@ -328,11 +319,11 @@ func (s *DB) SaveIssue(ctx context.Context, repositoryOwner, repositoryName stri
 	hash := sha256.Sum256([]byte(st))
 	hashString := fmt.Sprintf("%x", hash)
 
-	closedById := 0
+	closedByID := 0
 	closedByLogin := ""
 
 	if len(issue.ClosedBy.Nodes) > 0 {
-		closedById = issue.ClosedBy.Nodes[0].ClosedEvent.Actor.DatabaseId
+		closedByID = issue.ClosedBy.Nodes[0].ClosedEvent.Actor.DatabaseID
 		closedByLogin = issue.ClosedBy.Nodes[0].ClosedEvent.Actor.Login
 	}
 
@@ -343,24 +334,24 @@ func (s *DB) SaveIssue(ctx context.Context, repositoryOwner, repositoryName stri
 		pq.Array(assignees),          // assignees text[] NOT NULL,
 		issue.Body,                   // body text,
 		issue.ClosedAt,               // closed_at timestamptz,
-		closedById,                   // closed_by_id bigint NOT NULL
+		closedByID,                   // closed_by_id bigint NOT NULL
 		closedByLogin,                // closed_by_login text NOT NULL,
 		issue.Comments.TotalCount,    // comments bigint,
 		issue.CreatedAt,              // created_at timestamptz,
-		issue.Url,                    // htmlurl text,
-		issue.DatabaseId,             // id bigint,
+		issue.URL,                    // htmlurl text,
+		issue.DatabaseID,             // id bigint,
 		pq.Array(labels),             // labels text[] NOT NULL,
 		issue.Locked,                 // locked boolean,
-		issue.Milestone.Id,           // milestone_id text NOT NULL,
+		issue.Milestone.ID,           // milestone_id text NOT NULL,
 		issue.Milestone.Title,        // milestone_title text NOT NULL,
-		issue.Id,                     // node_id text,
+		issue.ID,                     // node_id text,
 		issue.Number,                 // number bigint,
 		repositoryName,               // repository_name text NOT NULL,
 		repositoryOwner,              // repository_owner text NOT NULL,
 		issue.State,                  // state text,
 		issue.Title,                  // title text,
 		issue.UpdatedAt,              // updated_at timestamptz,
-		issue.Author.User.DatabaseId, // user_id bigint NOT NULL,
+		issue.Author.User.DatabaseID, // user_id bigint NOT NULL,
 		issue.Author.Login,           // user_login text NOT NULL,
 
 		s.v,
@@ -392,14 +383,14 @@ func (s *DB) SaveIssueComment(ctx context.Context, repositoryOwner, repositoryNa
 		comment.AuthorAssociation,      // author_association text,
 		comment.Body,                   // body text,
 		comment.CreatedAt,              // created_at timestamptz,
-		comment.Url,                    // htmlurl text,
-		comment.DatabaseId,             // id bigint,
+		comment.URL,                    // htmlurl text,
+		comment.DatabaseID,             // id bigint,
 		issueNumber,                    // issue_number bigint NOT NULL,
-		comment.Id,                     // node_id text,
+		comment.ID,                     // node_id text,
 		repositoryName,                 // repository_name text NOT NULL,
 		repositoryOwner,                // repository_owner text NOT NULL,
 		comment.UpdatedAt,              // updated_at timestamptz,
-		comment.Author.User.DatabaseId, // user_id bigint NOT NULL,
+		comment.Author.User.DatabaseID, // user_id bigint NOT NULL,
 		comment.Author.Login,           // user_login text NOT NULL,
 
 		s.v,
@@ -451,19 +442,19 @@ func (s *DB) SavePullRequest(ctx context.Context, repositoryOwner, repositoryNam
 		pr.HeadRef.Repository.Owner.Login, // head_repository_owner text NOT NULL,
 		pr.HeadRef.Target.Oid,             // head_sha text NOT NULL,
 		pr.HeadRef.Target.Commit.Author.User.Login, // head_user text NOT NULL,
-		pr.Url,                      // htmlurl text,
-		pr.DatabaseId,               // id bigint,
+		pr.URL,                      // htmlurl text,
+		pr.DatabaseID,               // id bigint,
 		pq.Array(labels),            // labels text[] NOT NULL,
 		pr.MaintainerCanModify,      // maintainer_can_modify boolean,
 		pr.MergeCommit.Oid,          // merge_commit_sha text,
 		pr.Mergeable == "MERGEABLE", // mergeable boolean,
 		pr.Merged,                   // merged boolean,
 		pr.MergedAt,                 // merged_at timestamptz,
-		pr.MergedBy.DatabaseId,      // merged_by_id bigint NOT NULL,
+		pr.MergedBy.DatabaseID,      // merged_by_id bigint NOT NULL,
 		pr.MergedBy.Login,           // merged_by_login text NOT NULL,
-		pr.Milestone.Id,             // milestone_id text NOT NULL,
+		pr.Milestone.ID,             // milestone_id text NOT NULL,
 		pr.Milestone.Title,          // milestone_title text NOT NULL,
-		pr.Id,                       // node_id text,
+		pr.ID,                       // node_id text,
 		pr.Number,                   // number bigint,
 		repositoryName,              // repository_name text NOT NULL,
 		repositoryOwner,             // repository_owner text NOT NULL,
@@ -471,7 +462,7 @@ func (s *DB) SavePullRequest(ctx context.Context, repositoryOwner, repositoryNam
 		pr.State,                    // state text,
 		pr.Title,                    // title text,
 		pr.UpdatedAt,                // updated_at timestamptz,
-		pr.Author.DatabaseId,        // user_id bigint NOT NULL,
+		pr.Author.DatabaseID,        // user_id bigint NOT NULL,
 		pr.Author.Login,             // user_login text NOT NULL,
 
 		s.v,
@@ -507,15 +498,15 @@ func (s *DB) SavePullRequestReview(ctx context.Context, repositoryOwner, reposit
 
 		review.Body,                   // body text,
 		review.Commit.Oid,             // commit_id text,
-		review.Url,                    // htmlurl text,
-		review.DatabaseId,             // id bigint,
-		review.Id,                     // node_id text,
+		review.URL,                    // htmlurl text,
+		review.DatabaseID,             // id bigint,
+		review.ID,                     // node_id text,
 		pullRequestNumber,             // pull_request_number bigint NOT NULL,
 		repositoryName,                // repository_name text NOT NULL,
 		repositoryOwner,               // repository_owner text NOT NULL,
 		review.State,                  // state text,
 		review.SubmittedAt,            // submitted_at timestamptz,
-		review.Author.User.DatabaseId, // user_id bigint NOT NULL,
+		review.Author.User.DatabaseID, // user_id bigint NOT NULL,
 		review.Author.Login,           // user_login text NOT NULL,
 
 		s.v,
@@ -550,11 +541,11 @@ func (s *DB) SavePullRequestReviewComment(ctx context.Context, repositoryOwner, 
 		comment.Commit.Oid,        // commit_id text,
 		comment.CreatedAt,         // created_at timestamptz,
 		comment.DiffHunk,          // diff_hunk text,
-		comment.Url,               // htmlurl text,
-		comment.DatabaseId,        // id bigint,
+		comment.URL,               // htmlurl text,
+		comment.DatabaseID,        // id bigint,
 		// TODO
 		0,                          // in_reply_to bigint,
-		comment.Id,                 // node_id text,
+		comment.ID,                 // node_id text,
 		comment.OriginalCommit.Oid, // original_commit_id text,
 		comment.OriginalPosition,   // original_position bigint,
 		comment.Path,               // path text,
@@ -564,7 +555,7 @@ func (s *DB) SavePullRequestReviewComment(ctx context.Context, repositoryOwner, 
 		repositoryName,             // repository_name text NOT NULL,
 		repositoryOwner,            // repository_owner text NOT NULL,
 		comment.UpdatedAt,          // updated_at timestamptz,
-		comment.Author.DatabaseId,  // user_id bigint NOT NULL,
+		comment.Author.DatabaseID,  // user_id bigint NOT NULL,
 		comment.Author.Login,       // user_login text NOT NULL,
 
 		s.v,
